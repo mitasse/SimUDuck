b09c56a3dab2526b414e94a46fe4d4b26feebdc2
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-05 16:19:58 +0100

    feat: fly rocket powered behavior

---
+++ fly_behavior/fly_rocket_powered.py
@@ -0,0 +1,9 @@
+"""Represent a fly behavior"""
+
+
+class FlyRocketPowered:
+    """Fly rocket powered concrete fly behavior"""
+
+    def fly(self) -> None:
+        """Fly"""
+        print("I 'm flying with a rocket!'")
