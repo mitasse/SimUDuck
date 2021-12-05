68933eb0dd01b46a83c975dbc83d2cdbc1ab520a
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-05 16:21:32 +0100

    fix: use of dataclass for mallard concrete duck

--- ducks/mallard.py
+++ ducks/mallard.py
@@ -1,16 +1,19 @@
 """Represent a duck class"""
+from dataclasses import dataclass
+
 from fly_behavior.fly_with_wings import FlyWithWings
 from quack_behavior.quack import Quack
 
 from ducks.duck import Duck
 
 
+@dataclass
 class MallardDuck(Duck):
     """Mallard concrete duck class"""
 
     quack_behavior = Quack()
     fly_behavior = FlyWithWings()
 
-    def display(self):
+    def display(self) -> None:
         """Display"""
         print("I'm a real Mallard duck")
