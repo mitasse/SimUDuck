e026edbc0a3339ddfc68a39e6f552af7c0e8a70f
Maxime Itasse <maxime.itasse@gmail.com>
2021-12-05 15:40:46 +0100

    feat: fly with wings behavior

---
+++ fly_behavior/fly_with_wings.py
@@ -0,0 +1,9 @@
+"""Represent a fly behavior"""
+
+
+class FlyWithWings:
+    """Fly with wings concrete fly behavior"""
+
+    def fly(self) -> None:
+        """Fly"""
+        print("I'm flying!!")
